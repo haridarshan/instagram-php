20c096f6d788fcfbda702b59d6f4b3919b8b9819
haridarshan <er.haridarshan@gmail.com>
2016-05-10 15:40:12 +0530

    fix

--- src/InstagramException.php
+++ src/InstagramException.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 namespace Haridarshan\Instagram;
-=======
-namespace haridarshan\Instagram;
->>>>>>> d70794b1756c72ede570206aa374b6e04ad591ec
 
 class InstagramException extends \Exception{
 	
