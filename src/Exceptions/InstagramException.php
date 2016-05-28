1222165eb14ce737a08d7f3bc940fd5c9dccf9da
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-05-28 21:02:25 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Exceptions/InstagramException.php
+++ src/Exceptions/InstagramException.php
@@ -3,13 +3,13 @@ namespace Haridarshan\Instagram\Exceptions;
 
 class InstagramException extends \Exception
 {
-    /*
+	/*
     * Get Exception type
     * @return string | nul;
     */
-    public function getType()
-    {
-        $message = json_decode($this->message);
-        return isset($message->Type) ? $message->Type : null;
-    }
+	public function getType()
+	{
+		$message = json_decode($this->message);
+		return isset($message->Type) ? $message->Type : null;
+	}
 }
